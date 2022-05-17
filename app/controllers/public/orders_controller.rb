4cc311380632e8af1a86c497c2fe3ef0f5442b02
EC2 Default User <takahito12rai@gmail.com>
2022-05-17 07:30:34 +0000

    [create]routes/controllers

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,19 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+
+  def confirmation
+  end
+
+  def completed
+  end
+
+  def index
+  end
+
+  def show
+  end
+  
+  def create
+  end 
+end
