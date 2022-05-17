85e1720a258d8e897f3f59a619ab712aab539021
aknotj <100099666+aknotj@users.noreply.github.com>
2022-05-17 06:20:39 +0000

    Models Validation/association

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,18 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  has_many :shipping_addresses, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kane, presence: true
+  validates :first_name_kana, presence: true
+  validates :postal_code, presence: true, length: {maximum: 7}
+  validates :address, presence: true
+  validates :telephone_number, presence: true, length: {maximum: 11}
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :order_details, dependent: :destroy
+  has_many :items, through: :order_details
+  
+  validates :postal_code, presence: true, length: {maximum: 7}
+  validates :address, presence: true
+  validates :name, presence: true
+
 end
