82a40533644425bc6a255436317f3746a2e5f011
aknotj <100099666+aknotj@users.noreply.github.com>
2022-05-17 06:35:05 +0000

    typo fixed 2

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_one_attached :item_image
   
-  validates :item_name, presence: true
+  validates :name, presence: true
   validates :introduction, presence: true
   
   def get_item_image(width, height)
