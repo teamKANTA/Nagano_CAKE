8a92e735f65cd77e88867d3da1979a49a80fd9cc
aknotj <100099666+aknotj@users.noreply.github.com>
2022-05-17 22:35:42 +0000

    devise customerログイン時Strong parameter追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def after_sign_in_path_for(resource)
+    items_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :email, :telephone_number])
+  end
+  
 end
