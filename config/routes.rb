e78427abab72a7dd333e0f0dac23e43fa24288a1
aknotj <100099666+aknotj@users.noreply.github.com>
2022-05-18 00:23:55 +0000

    fix: customersルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,6 @@
 Rails.application.routes.draw do
 
-    
 
-  namespace :admin do
-    resources :customers, only: [:index, :show, :edit, :update]
-    
-    resources :genres, only: [:index, :create, :edit, :update]
-    
-    resources :items, except: [:destroy]
-    
-    resources :orders, only: [:index, :show, :update]
-    
-    resources :order_details, only: [:update]
-  end
-
-  scope module: :public do
-    root to: 'homes#top'
-    get 'about' => 'homes#about'
-    resources :items, only: [:index, :show]
-    
-    resources :customers, only: [:show, :edit, :update]
-    get 'customers/quit', as: 'quit'
-    patch 'customers/withdraw', as: 'withdraw'
-    
-    resources :shipping_addresses, only: [:create, :index, :edit, :update, :destroy]
-    
-    resources :cart_items, only: [:index, :create, :update, :destroy]
-    delete 'cart_items/empty', as: 'empty'
-    
-    resources :orders, only: [:new, :create, :index, :show]
-    get 'orders/completed', as: 'completed'
-    post 'orders/confirmation', as: 'confirmation'
-  end
-  
-  
-  
-  
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -44,6 +9,42 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
+
+  namespace :admin do
+    resources :customer, only: [:index, :show, :edit, :update]
+
+    resources :genres, only: [:index, :create, :edit, :update]
+
+    resources :items, except: [:destroy]
+
+    resources :orders, only: [:index, :show, :update]
+
+    resources :order_details, only: [:update]
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    resources :items, only: [:index, :show]
+
+    resource :customers, only: [:show, :edit, :update]
+    get 'customers/quit', as: 'quit'
+    patch 'customers/withdraw', as: 'withdraw'
+
+    resources :shipping_addresses, only: [:create, :index, :edit, :update, :destroy]
+
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+    delete 'cart_items/empty', as: 'empty'
+
+    resources :orders, only: [:new, :create, :index, :show]
+    get 'orders/completed', as: 'completed'
+    post 'orders/confirmation', as: 'confirmation'
+  end
+
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
