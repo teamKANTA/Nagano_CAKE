6f58dd0f262e086bbe3a888454d06fac9774dcdc
takekai822 <kaikisoccer78@gmail.com>
2022-05-18 01:35:54 +0000

    コントローラーに記述を追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,43 @@
 class Public::CartItemsController < ApplicationController
-  def index
-  end
-  
+  #before_action :authenticate_user!
+
+
   def create
+    @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+    if @cart_item.present?
+      @cart_item.quantity += params[:cart_item][:quantity].to_i
+    else
+      @cart_item = current_customer.cart_items.new(cart_item_params)
+    end
+
+    if @cart_item.save
+      redirect_to orders_path
+    end
   end
-  
+
+  def index
+    @cart_items = current_customer.cart_items
+  end
+
   def update
-  end 
-  
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
+  end
+
   def destroy
-  end 
-  
+    @cart_item = CartItem.find(parmas[:id])
+    @cart_item.destroy
+  end
+
   def empty
-  end 
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy
+    redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :quantity)
+  end
 end
