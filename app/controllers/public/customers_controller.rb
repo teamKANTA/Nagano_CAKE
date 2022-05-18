3fba185ab9dddc4a718d0e7e50dc9d54049b1b36
Naohiroariga <yukiyanagi.bs@gmail.com>
2022-05-18 06:06:55 +0000

    [Update]customer editページ更新

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,16 +5,26 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
-
   end
 
   def quit
   end
 
   def update
+    if current_customer.update(customer_params)
+      redirect_to customer_path
+    else
+      render "edit"
+    end
   end
 
   def withdraw
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 
 end
