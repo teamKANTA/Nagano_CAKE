ea68f81561f277e45c9ca917fb2b3265f30f2d29
Naohiroariga <yukiyanagi.bs@gmail.com>
2022-05-18 04:02:54 +0000

    [Add]customer_edit追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,20 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
   def show
   end
 
   def edit
+
   end
 
   def quit
   end
-  
+
   def update
-  end 
-  
+  end
+
   def withdraw
-  end 
+  end
+
 end
