c653be63cff6294459fd002a84d30b7e1c71af02
aknotj <100099666+aknotj@users.noreply.github.com>
2022-05-18 23:59:58 +0000

    注文機能 new~completed

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,19 +1,50 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+    @customer = current_customer
   end
 
   def confirmation
+    @order = Order.new(order_params)
+    @customer = current_customer
+    @payment_method = @order.method_of_payment
+    #会員に登録された住所
+    if params[:order][:shipping_address] == "my_address"
+      @order.postal_code = @customer.postal_code
+      @order.address = @customer.address
+      @order.name = @customer.family_name + @customer.first_name
+    #登録済み住所から選択された住所
+    elsif params[:order][:shipping_address] == "registered_address"
+      @address = ShippingAddress.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    end
+    @order.save
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+    redirect_to completed_path
   end
 
   def completed
   end
 
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
   
-  def create
-  end 
+  private
+  def order_params
+    params.require(:order).permit(:method_of_payment, :shipping_address, :postal_code, :address, :name)
+  end
+
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,12 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
   has_many :items, through: :order_details
-  
+
   validates :postal_code, presence: true, length: {maximum: 7}
   validates :address, presence: true
   validates :name, presence: true
+
+  enum method_of_payment: {credit_card: 0, transfer: 1}
+  enum shipping_address: {my_address: 0, registered_address: 1, new_address: 2}
 
 end
