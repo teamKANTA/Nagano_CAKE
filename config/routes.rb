b288b2498738d08c8427bd2c4d1e41fa63f978bd
aknotj <100099666+aknotj@users.noreply.github.com>
2022-05-18 01:09:39 +0000

    customersルーティング記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
     get 'about' => 'homes#about'
     resources :items, only: [:index, :show]
 
-    resources :customers, only: [:show, :edit, :update]
+    resource :customers, only: [:show, :edit, :update]
     get 'customers/quit', as: 'quit'
     patch 'customers/withdraw', as: 'withdraw'
 
