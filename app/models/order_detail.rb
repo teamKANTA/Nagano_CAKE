f44ca77dd7383c2d96f276ace708ac27f63c56b0
aknotj <100099666+aknotj@users.noreply.github.com>
2022-05-19 04:38:33 +0000

    order_details ver.1

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,8 @@
 class OrderDetail < ApplicationRecord
+  
   belongs_to :order
   belongs_to :item
+  
+  enum production_status: {pending: 0, waiting_for_production: 1, working: 2, completed: 3}
 
 end
