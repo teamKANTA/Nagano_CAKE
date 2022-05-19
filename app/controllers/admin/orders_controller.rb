4aea0659e4f754c8ef88ffb8f3bec6431043d9fa
aknotj <100099666+aknotj@users.noreply.github.com>
2022-05-19 04:41:04 +0000

    controllers

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,17 +7,17 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_customer_name = @order.customer.family_name + @order.customer.first_name
   end
-  
+
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
     redirect_to admin_order_path(@order)
   end
-  
+
   private
-  
+
   def order_params
     params.require(:order).permit(:order_status)
   end
-  
+
 end
