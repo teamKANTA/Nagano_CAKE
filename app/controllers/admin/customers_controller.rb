d78b870d37e6ea2ebaf3bc16ffb2084f46b48ad9
Naohiroariga <yukiyanagi.bs@gmail.com>
2022-05-20 00:27:15 +0000

    [Update]update追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,6 +14,17 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
-    
+    @customer =Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), notice: "会員情報を更新しました。"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
 end
