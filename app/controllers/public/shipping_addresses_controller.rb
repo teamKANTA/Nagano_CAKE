d80ca065b49dafc18f9211d5b8776f54078684b6
Naohiroariga <yukiyanagi.bs@gmail.com>
2022-05-21 13:00:29 +0000

    [Update]非同期通信追加

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,6 +1,6 @@
 class Public::ShippingAddressesController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def index
     @address_date = ShippingAddress.new
     @address_dates = current_customer.shipping_addresses
@@ -14,12 +14,9 @@ class Public::ShippingAddressesController < ApplicationController
   def create
     @address_date = ShippingAddress.new(address_date_params)
     @address_date.customer_id = current_customer.id
-    if @address_date.save
-      redirect_to shipping_addresses_path, notice: "配送先を登録しました。"
-    else
-      @address_dates = current_customer.shipping_addresses
-      render "index"
-    end
+    @address_dates = current_customer.shipping_addresses
+    @address_date.save
+
   end
 
   def update
@@ -34,7 +31,8 @@ class Public::ShippingAddressesController < ApplicationController
   def destroy
     @address_date = ShippingAddress.find(params[:id])
     @address_date.destroy
-    redirect_to shipping_addresses_path, notice: "配送先を削除しました。"
+    @address_dates = current_customer.shipping_addresses
+
   end
 
   private
