fce4dd103ab88a4197fd5e0c051fcfbe3bc602c0
EC2 Default User <takahito12rai@gmail.com>
2022-05-24 03:34:18 +0000

    ジャンル検索アクションコメント

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -12,11 +12,11 @@ class Public::ItemsController < ApplicationController
   end
 
   def search
-    #binding.pry
-    @items = Item.where(genre_id: params[:format]).page(params[:page]).per(8)
-    @quantity = Item.where(genre_id: params[:format]).count
+    #ジャンル検索アクション
+    @items = Item.where(genre_id: params[:format]).page(params[:page]).per(8)#パラメーターで取得したジャンルIDをもとにgenre_idと一致する商品の取得
+    @quantity = Item.where(genre_id: params[:format]).count#検索してきた商品数をカウント
     @genres = Genre.all
-    @genre = Genre.find(params[:format])
+    @genre = Genre.find(params[:format])#ビューで検索したジャンル名を表示するため変数を用意
     render 'index'
   end
 end
