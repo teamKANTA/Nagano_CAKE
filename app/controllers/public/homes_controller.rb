c701b4ea064698904ba38f04170a75edfafbabf6
takekai822 <kaikisoccer78@gmail.com>
2022-05-24 05:34:15 +0000

    微調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,7 @@ class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
     #登録された商品を新しい順に4つ取得
-    @items = Item.order('id DESC').limit(4)
+    @items = Item.where(is_active: true).order('id DESC').limit(4)
   end
 
   def about
